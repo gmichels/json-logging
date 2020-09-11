baec3d1cafb738aafcae7f7782b0339212939922
Gustavo Michels <gmichels@nanthealth.com>
2020-09-11 15:09:08 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime
+import logging
+import sys
+
+import flask
+import json_logging
+
+app = flask.Flask(__name__)
+json_logging.init_flask(enable_json=True)
+json_logging.init_request_instrument(app)
+
+# init the logger as usual
+logger = logging.getLogger("test-logger")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+@app.route('/')
+def home():
+    logger.info("test log statement")
+    logger.info("test log statement with extra props", extra={'props': {"extra_property": 'extra_value'}})
+    correlation_id = json_logging.get_correlation_id()
+    return "Hello world : " + str(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(5000), use_reloader=False)
+
